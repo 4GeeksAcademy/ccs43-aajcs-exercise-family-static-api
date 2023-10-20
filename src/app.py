3334aa65cb796b064eda4b641f9a7c65ce32f204
Alfredo Castillo <98472648+aajcs@users.noreply.github.com>
2023-10-20 10:13:25 +0000

    subiendo para entrega

--- src/app.py
+++ src/app.py
@@ -25,18 +25,49 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_members():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
+    
+
+@app.route('/members', methods=['GET'])
+def get_all_members():
+    members = jackson_family.get_all_members()
+    return jsonify(members), 200
 
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member is None:
+        return jsonify({"error": "Member not found"}), 404
+    return jsonify(member), 200
+
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    new_member_data = request.get_json()
+    new_member_id = new_member_data.get('id', None)
+    if new_member_id is None:
+        new_member_id = jackson_family._generateId()
+    new_member_data['id'] = new_member_id
+
+    jackson_family.add_member(new_member_data)
+
+    members = jackson_family.get_all_members()
+    return jsonify({"message": "Member added successfully", "count": len(members)}), 200
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    if jackson_family.delete_member(member_id):
+        members = jackson_family.get_all_members()
+        return jsonify({"done": True, "count": len(members)}), 200
+    else:
+        return jsonify({"error": "Member not found"}), 404
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
